1f261161328282402e4037d628f7c7f31bba7297
sar-bc@mail.ru <sar-bc@mail.ru>
2026-07-15 21:45:23 +0400

    add init sql

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,8 +1,13 @@
+-- Создаем базу данных
 CREATE DATABASE IF NOT EXISTS bot_doctor_max;
-USE bot_doctor_max;
 
+-- Создаем пользователя doctor с доступом с любого хоста
 CREATE USER IF NOT EXISTS 'doctor'@'%' IDENTIFIED BY '159753';
 GRANT ALL PRIVILEGES ON bot_doctor_max.* TO 'doctor'@'%';
 
+-- Создаем root с доступом с любого хоста
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '159753';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+
+-- Применяем изменения
+FLUSH PRIVILEGES;
